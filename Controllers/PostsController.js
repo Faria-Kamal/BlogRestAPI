a4595dc68c2463828efae283daed0aaa7fa288d1
zer0-byte0 <k.fariasoha@gmail.com>
2022-05-09 15:30:02 +0600

    createPost method implemented

--- Controllers/PostsController.js
+++ Controllers/PostsController.js
@@ -1,3 +1,4 @@
+const req = require("express/lib/request")
 const mysql = require("mysql")
 
 
@@ -8,6 +9,34 @@ function getAllPosts(req, res){
         res.json(row)
     })
 }
+
+// function getPostsByID(req, res){
+//     const id = req.params
+//     getConnection().query(`SELECT * FROM posts WHERE id=${id}`, (err, row, fields) => {
+//         res.json(row)
+//     })
+// }
+
+ function createPosts(req, res){
+    const {category, title, content} =req.body
+    const queryStrings= "INSERT INTO posts (title, category, content) VALUES (?, ?, ?)"
+    getConnection().query(queryStrings, [title, category, content], (err, row, fields) => {
+        if(err){
+            res.json({
+                "status": err.toString()
+                
+              })
+              
+            return
+            } else {
+              res.send({
+                status: "post created"
+              });
+            }
+            
+            res.end()
+        })
+};
 
 
 const pool = mysql.createPool({
@@ -24,5 +53,8 @@ function getConnection() {
 
 module.exports = {
     getAllPosts,
+    // getPostsByID
+    createPosts
+
     
 }
--- Routers/PostsRouter.js
+++ Routers/PostsRouter.js
@@ -3,10 +3,15 @@ const router = require("express").Router();
 
 const{
     getAllPosts,
+    // getPostsByID,
+    createPosts,
+
 
 } = require('../Controllers/PostsController')
 
 router.get("/all", getAllPosts)
+// router.get("/id", getPostsByID)
+router.post("/create", createPosts)
 
 module.exports = router
 
